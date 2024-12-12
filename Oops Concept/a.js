44922a626a6450bf6c7f877ae16b54717ff9369c
nibedita0007 <biswasnibedita328@gmail.com>
2024-12-12 17:01:15 +0530

    done

--- Oops Concept/a.js
+++
@@ -1,2 +0,0 @@
-let arr = [10, 20, 30, 40, 50]
-console.log(typeof(arr));
--- Oops Concept/abstraction.py
+++ Oops Concept/abstraction.py
@@ -1,4 +1,31 @@
-import first,second
+# import first,second
 
-first.add()
-second.sub()
+# first.add()
+# second.sub()
+
+# Abstraction
+
+from abc import ABC,abstractmethod
+class Car(ABC):
+    def show(self):
+        print("Evry car has 4 wheels")
+    @abstractmethod
+    def Speed(self):
+        pass
+
+class Suzuki(Car):
+    def Speed(self):
+        print("speed is 100km/h")
+
+class Maruti(Car):
+    def Speed(self):
+        print("speed is 80km/h")
+
+obj1=Suzuki()
+obj1.show()
+obj1.Speed()
+
+obj2=Maruti()
+obj2.show()
+obj2.Speed()
+        
